2029b449ed097b3f8480d3d00d0f212389a6c3df
y2sman <leekjin97@gmail.com>
2021-07-02 16:59:16 +0900

    solved.ac CLASS3 금장 돌파용 알고리즘 문제 풀이 업로드 (2021.07.02 에 푼 것)

---
+++ baekjoon/[2] silver/11403_경로 찾기/11403_경로 찾기.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <queue>
+#include <utility>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <tuple>
+
+using namespace std;
+
+int N;
+
+int map[110][110];
+int visited[110][110];
+bool tmp_visited[110];
+
+void deep_search(int node)
+{
+  for(int i=0;i<N;i++)
+  {
+    if(map[node][i] == 1 && tmp_visited[i] == false)
+    {
+      tmp_visited[i] = true;
+      deep_search(i);
+    }
+  }
+}
+
+void search()
+{
+  for(int i=0;i<N;i++)
+  {
+    memset(tmp_visited,false,sizeof(tmp_visited));
+    deep_search(i);
+    for(int j=0;j<N;j++)
+    {
+      if(tmp_visited[j] == true)
+        visited[i][j] = 1;
+    }
+  }
+}
+
+int main()
+{
+  memset(map,0,sizeof(map));
+  memset(visited,0,sizeof(visited));
+
+  scanf("%d",&N);
+
+  for(int i=0;i<N;i++)
+  {
+    for(int j=0;j<N;j++)
+    {
+      int tmp;
+      scanf("%d",&tmp);
+      map[i][j] = tmp;
+    }
+  }
+
+  search();
+
+  for(int i=0;i<N;i++)
+  {
+    for(int j=0;j<N;j++)
+    {
+      printf("%d ",visited[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
+
+/*
+4
+0 1 1 0
+1 0 1 1
+1 1 0 0
+0 1 0 0
+
+무한호출 케이스
+*/
